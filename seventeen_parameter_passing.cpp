66d64ca06eef1342725d0d34d6b58560c126af9e
Girish Sharma <sharmagirish.grsh@gmail.com>
2025-12-23 05:00:29 +0530

    functions

--- seventeen_parameter_passing.cpp
+++ seventeen_parameter_passing.cpp
@@ -1,10 +1,45 @@
 #include <iostream>
 using namespace std;
 
-void swap(int a, int b) // PASS BY VALUE
+// CALL BY VALUE
+// void swap(int a, int b) // PASS BY VALUE
+// {
+//     int temp;
+//     temp = a;
+//     a = b;
+//     b = temp;
+// }
+
+// int main()
+// {
+//     int x = 10, y = 20;
+//     swap(x, y);
+//     cout << x << " " << y << endl; // values dont change in swap when value of actual parameters
+// }
+
+// CALL BY ADDRESS
+
+// int swap(int *a, int *b)
+// {
+//     int temp;
+//     temp = *a;
+//     *b = *a;
+//     *a = temp;
+// }
+
+// int main()
+// {
+//     int x = 10, y = 20;
+//     swap(&x, &y);
+//     cout << x << " " << y << endl; // VALUE CHANGES IN SWAP WHEN CALL BY ADDRESS
+// }
+
+// CALL BY REFERENCE
+
+int swap(int &a, int &b) // the & is making them references
 {
-    int temp;
-    temp = a;
+    cout << &a << " " << &b << endl; // gives same address as x and y
+    int temp = a;
     a = b;
     b = temp;
 }
@@ -12,6 +47,7 @@ void swap(int a, int b) // PASS BY VALUE
 int main()
 {
     int x = 10, y = 20;
-    swap(x, y);
-    cout << x << " " << y << endl; // values dont change in swap by value of actual parameters
+    swap(x, y); // dont need to pass address
+    cout << x << " " << y << endl;
+    cout << &x << " " << &y << endl; // gives same address as a and b
 }
